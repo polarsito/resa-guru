a10eab3595422ea4df76bb48873be5ee9487f9a5
punz <facundobolischki1@gmail.com>
2023-12-19 03:18:54 -0300

    deleted a.ts

--- a.ts
+++
@@ -1,5 +0,0 @@
-import { join } from 'path';
-import { getTokenGDrive } from 'utils-google-drive';
-getTokenGDrive({
-  pathCredentials: join(process.cwd(), 'credentials', 'drive-credentials.json'),
-});
